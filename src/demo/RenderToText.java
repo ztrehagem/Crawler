a84a3f75efbd1b629286e7595dda00a85185c5ff
MHz <megahertz.556@gmail.com>
2016-01-07 00:27:40 +0900

    add demos

---
+++ src/demo/RenderToText.java
@@ -0,0 +1,21 @@
+package demo;
+
+import java.net.URL;
+import net.htmlparser.jericho.Source;
+
+public class RenderToText {
+
+	public static void main( String[] args ) throws Exception {
+		String sourceUrlString = "http://megahertz.michikusa.jp";
+		if( args.length == 0 )
+			System.err.println( "Using default argument of \"" + sourceUrlString + '"' );
+		else
+			sourceUrlString = args[0];
+		if( sourceUrlString.indexOf( ':' ) == -1 )
+			sourceUrlString = "file:" + sourceUrlString;
+		Source source = new Source( new URL( sourceUrlString ) );
+		String renderedText = source.getRenderer().toString();
+		System.out.println( "\nSimple rendering of the HTML document:\n" );
+		System.out.println( renderedText );
+	}
+}
