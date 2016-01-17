dfed2ff06124aa41b75dedcff21b0a80e2ad011a
MHz <megahertz.556@gmail.com>
2016-01-17 23:46:31 +0900

    リファクタリング

--- src/crawler/one/Single.java
+++ src/crawler/one/Single.java
@@ -8,12 +8,9 @@ import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import debug.Log;
 import net.htmlparser.jericho.Attribute;
-import net.htmlparser.jericho.Attributes;
 import net.htmlparser.jericho.Element;
 import net.htmlparser.jericho.HTMLElementName;
 import net.htmlparser.jericho.OutputDocument;
@@ -21,32 +18,32 @@ import net.htmlparser.jericho.Source;
 
 public class Single {
 
-	private final File			localDir;
-	private final URL			url;
-	private Map<String, String>	extfilemap;
-
-	private int		fileId		= 0;
-	private Object	fileIdMutex	= new Object();
+	private final File	dir;
+	private final URL	url;
 
 	private Source			src;
 	private OutputDocument	od;
 
+	private int		fileId		= 0;
+	private Object	fileIdMutex	= new Object();
+
+	private Map<String, String> extfilemap;
+
 	public Single( String url ) throws Exception {
-		localDir = new File( "result/" + String.valueOf( System.currentTimeMillis() ) );
-		localDir.mkdirs();
-		Log.v( getClass(), "dir '" + localDir.getAbsolutePath() + "'" );
+		this.dir = new File( "result/" + String.valueOf( System.currentTimeMillis() ) );
+		this.dir.mkdirs();
+		Log.v( getClass(), "dir '" + this.dir.getAbsolutePath() + "'" );
 
 		this.url = new URL( url );
-		Log.v( getClass(), "target '" + url + "'\nhost '" + this.url.getHost() + "'\n" + "path '" + this.url.getPath()
-			+ "'\n" + "file '" + this.url.getFile() + "'\nprotocol '" + this.url.getProtocol() + "'" );
+		Log.v( getClass(), "target '" + url + "'" );
 
-		extfilemap = new HashMap<>();
+		this.extfilemap = new HashMap<>();
 	}
 
 	public void process() throws Exception {
 
-		src = new Source( url );
-		od = new OutputDocument( src );
+		this.src = new Source( this.url );
+		this.od = new OutputDocument( this.src );
 
 		saveExternals();
 
@@ -58,11 +55,9 @@ public class Single {
 		searchExternals( HTMLElementName.SCRIPT, "src" );
 		searchExternals( HTMLElementName.IMG, "src" );
 
-		int id = 0;
-		Set<String> keyset = extfilemap.keySet();
-		for( String path : keyset ) {
+		for( String path : extfilemap.keySet() ) {
 			try {
-				save( new File( localDir, extfilemap.get( path ) ), new URL( path ) );
+				save( new File( dir, extfilemap.get( path ) ), new URL( path ) );
 			}
 			catch( IOException e ) {
 				e.printStackTrace();
@@ -71,15 +66,9 @@ public class Single {
 	}
 
 	private void searchExternals( String tag, String attrName ) {
-		List<Element> l = src.getAllElements( tag );
-		for( Element e : l ) {
-			Attributes attrs = e.getAttributes();
-			Attribute attr = attrs.get( attrName );
-			if( attr == null ) {
-				continue;
-			}
-			String path = attr.getValue();
-			Log.v( getClass(), path );
+		for( Element e : src.getAllElements( tag ) ) {
+
+			String path = e.getAttributeValue( attrName );
 			if( path == null || path.startsWith( "#" ) || path.startsWith( "javascript:" ) ) {
 				continue;
 			}
@@ -87,38 +76,36 @@ public class Single {
 			path = makeFullPath( path );
 
 			if( extfilemap.containsKey( path ) ) {
+				modifyRef( e, attrName, extfilemap.get( path ) );
 				continue;
 			}
 
-			int id;
-			synchronized( fileIdMutex ) {
-				id = ++fileId;
-			}
-			final String outputFilename = id + "." + getExtension( path );
-			extfilemap.put( path, outputFilename );
-
-			// 出力HTMLのhrefを書き換える // Map中被りにも適用しろ
-			String replaced = null;
 			{
-				OutputDocument od = new OutputDocument( e );
-				Log.v( getClass(), "od before '" + od.toString() + "'" );
-				HashMap<String, String> attrmap = new HashMap<>();
-				for( Attribute a : e.getAttributes() ) {
-					if( a.getName().equals( attrName ) )
-						continue;
-					attrmap.put( a.getName(), a.getValue() );
+				int id;
+				synchronized( fileIdMutex ) {
+					id = ++fileId;
 				}
-				attrmap.put( attrName, outputFilename );
-				od.replace( attrs, attrmap );
-				replaced = od.toString();
-				Log.v( getClass(), "od after '" + replaced + "'" );
+				final String outputFilename = id + "." + getExtension( path );
+				extfilemap.put( path, outputFilename );
+				modifyRef( e, attrName, outputFilename );
 			}
-			{
-				od.replace( e, replaced );
-			}
-
-			Log.v( getClass(), "add extfiles <" + tag + " " + attrName + "> '" + path + "'" );
 		}
+	}
+
+	private void modifyRef( final Element e, final String attrName, final String outputFilename ) {
+		OutputDocument od = new OutputDocument( e );
+
+		HashMap<String, String> attrmap = new HashMap<>();
+		for( Attribute a : e.getAttributes() ) {
+			if( a.getName().equals( attrName ) )
+				continue;
+			attrmap.put( a.getName(), a.getValue() );
+		}
+		attrmap.put( attrName, outputFilename );
+		od.replace( e.getAttributes(), attrmap );
+		final String replaced = od.toString();
+
+		this.od.replace( e, replaced );
 	}
 
 	private String makeFullPath( final String path ) {
@@ -126,14 +113,11 @@ public class Single {
 		if( path.contains( "//" ) ) { // 絶対パス
 			sb.append( path );
 			if( !path.contains( ":" ) ) {
-				sb.insert( 0, ":" );
-				sb.insert( 0, this.url.getProtocol() );
+				sb.insert( 0, ":" ).insert( 0, this.url.getProtocol() );
 			}
 		}
 		else { // 相対パス
-			sb.append( this.url.getProtocol() );
-			sb.append( "://" );
-			sb.append( this.url.getHost() );
+			sb.append( this.url.getProtocol() ).append( "://" ).append( this.url.getHost() );
 			if( path.startsWith( "/" ) ) { // ルートから
 				sb.append( path );
 			}
@@ -145,8 +129,7 @@ public class Single {
 				if( ts.length != 0 && ts[ts.length - 1].contains( "." ) ) {
 					StringBuilder tb = new StringBuilder();
 					for( int i = 0; i < ts.length - 1; i++ ) {
-						tb.append( ts[i] );
-						tb.append( "/" );
+						tb.append( ts[i] ).append( "/" );
 					}
 					t = tb.toString();
 				}
@@ -166,7 +149,7 @@ public class Single {
 	}
 
 	private void saveHTML() throws IOException {
-		final File file = new File( localDir.getCanonicalPath() + "/" + localDir.getName() + ".html" );
+		final File file = new File( dir, dir.getName() + ".html" );
 		if( !file.createNewFile() ) {
 			Log.e( getClass(), "failed create html file" );
 			return;
