580a8feeb380cd4acc462db54e62935975168b65
MHz <megahertz.556@gmail.com>
2016-01-18 13:47:37 +0900

    拡張子ではなくMIMEタイプを見ないといけない　　URLのコネクション数の制限が必要？

--- src/crawler/External.java
+++ src/crawler/External.java
@@ -1,10 +1,51 @@
 package crawler;
 
-public class External implements Runnable {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import debug.Log;
+
+class External implements Runnable {
+	// single External
+
+	private final File	file;
+	private final URL	url;
+
+	External( String url, File file ) throws MalformedURLException {
+		this.url = new URL( url );
+		this.file = file;
+	}
 
 	@Override
 	public void run() {
-		// TODO 自動生成されたメソッド・スタブ
+		Log.v( getClass(), "start save external '" + url + "' -> '" + file + "'" );
 
+		try {
+			if( !file.createNewFile() ) {
+				Log.e( getClass(), "This file is already exist '" + file + "'" );
+				return;
+			}
+			URLConnection c = url.openConnection();
+			InputStream in = c.getInputStream();
+			FileOutputStream out = new FileOutputStream( file );
+			byte[] buf = new byte[4096];
+			while( true ) {
+				int len = in.read( buf );
+				if( len <= 0 )
+					break;
+				out.write( buf, 0, len );
+			}
+			out.close();
+			in.close();
+
+			Log.v( getClass(), "save done '" + url + "' -> '" + file + "'" );
+		}
+		catch( Exception e ) {
+			Log.e( getClass(), "Exception on External '" + url + "'" );
+			e.printStackTrace();
+		}
 	}
 }
--- src/crawler/Master.java
+++ src/crawler/Master.java
@@ -1,7 +1,53 @@
 package crawler;
 
-public class Master {
+import java.io.File;
+import java.net.MalformedURLException;
+import java.util.HashMap;
+import java.util.Map;
+import debug.Log;
 
-	public Master( String url ) {
+public class Master {
+	// Master -> Page
+
+	private final File		root;
+	private final String	url;
+	private final int		h;
+
+	private final Map<String, Integer> pagemap;
+
+	private int pageIdCounter = 0;
+
+	public Master( String url, int h ) throws MalformedURLException {
+		this.root = new File( "result/" + String.valueOf( System.currentTimeMillis() ) );
+		this.root.mkdirs();
+		Log.v( getClass(), "dir '" + this.root.getAbsolutePath() + "'" );
+
+		this.url = url;
+		Log.v( getClass(), "target '" + url + "'" );
+
+		this.h = h;
+
+		this.pagemap = new HashMap<>();
+	}
+
+	public void process() throws MalformedURLException, InterruptedException {
+		Log.v( getClass(), "start" );
+		Thread t = new Thread( new Page( this, url, ++pageIdCounter, h - 1 ) );
+		t.start();
+		t.join();
+		Log.v( Main.class, "done!" );
+	}
+
+	File getRootDir() {
+		return this.root;
+	}
+
+	synchronized PageInfo addPageList( String url ) {
+		if( pagemap.containsKey( url ) ) {
+			return new PageInfo( pagemap.get( url ), true );
+		}
+
+		pagemap.put( url, ++pageIdCounter );
+		return new PageInfo( pageIdCounter, false );
 	}
 }
