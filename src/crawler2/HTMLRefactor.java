7dae4b619d73689af88ac62ce83dcff162a4face
MHz <megahertz.556@gmail.com>
2016-01-23 14:22:23 +0900

    Refactorをまとめてみた

--- src/crawler2/HTMLRefactor.java
+++ src/crawler2/HTMLRefactor.java
@@ -1,7 +1,8 @@
 package crawler2;
 
+import java.util.Map;
+import debug.Log;
 import net.htmlparser.jericho.Element;
-import net.htmlparser.jericho.HTMLElementName;
 import net.htmlparser.jericho.OutputDocument;
 import net.htmlparser.jericho.Source;
 
@@ -22,19 +23,73 @@ class HTMLRefactor {
 	}
 
 	private void refactoring() {
-		scan( HTMLElementName.IMG, new HTMLTagRefactorIMG( master, url, h, od ) );
-		scan( HTMLElementName.LINK, new HTMLTagRefactorLINK( master, url, h, od ) );
-		scan( HTMLElementName.SCRIPT, new HTMLTagRefactorSCRIPT( master, url, h, od ) );
-		scan( HTMLElementName.A, new HTMLTagRefactorA( master, url, h, od ) );
+		tag( "img", "src" );
+		tag( "link", "href" );
+		tag( "script", "src" );
+		tag( "a", "href" );
+		tag( "iframe", "src" );
 	}
 
 	OutputDocument getResult() {
 		return this.od;
 	}
 
-	private void scan( final String tag, final HTMLTagRefactor r ) {
+	private void tag( final String tag, final String attrname ) {
 		for( Element e : od.getSegment().getAllElements( tag ) ) {
-			r.refactoring( e );
+			final String path = e.getAttributeValue( attrname );
+			if( path == null )
+				continue;
+
+			final String fullpath = Tools.makeFullPath( url, path );
+			if( fullpath == null )
+				continue;
+
+			if( isHTMLLinkTag( tag ) ) {
+				if( !isHTML( tag, fullpath ) )
+					continue;
+				if( master.h.makeID( fullpath ) && h > 0 )
+					master.t.exec( new HTMLSaveRunner( master, fullpath, h ) );
+				this.modify( e, attrname, master.h.getFileName( fullpath ) );
+			}
+			else {
+				final TriBool t = master.f.makeID( fullpath, e );
+				if( t == TriBool.ERROR )
+					continue;
+				final boolean exist = t == TriBool.TRUE ? false : true;
+
+				if( !exist )
+					master.t.exec( new FileSaveRunner( master, fullpath ) );
+				this.modify( e, attrname, master.f.getFileName( fullpath ) );
+			}
+		}
+	}
+
+	private boolean isHTMLLinkTag( final String tag ) {
+		return in( tag, new String[] { "a", "iframe" } );
+	}
+
+	private boolean isHTML( final String tag, final String path ) {
+		if( !path.startsWith( "http:" ) && !path.startsWith( "https:" ) )
+			return false;
+		final String ext = Tools.getExtension( path );
+		return ext == null || in( ext, new String[] { "html", "htm", "asp", "aspx", "php" } );
+	}
+
+	private boolean in( final String s, final String[] a ) {
+		for( String as : a ) {
+			if( s.equals( as ) )
+				return true;
+		}
+		return false;
+	}
+
+	private void modify( final Element e, final String attrname, final String value ) {
+		try {
+			Map<String, String> map = od.replace( e.getAttributes(), true );
+			map.put( attrname.toLowerCase(), value );
+		}
+		catch( Exception exc ) {
+			Log.e( getClass(), "cant modifyRef : " + exc );
 		}
 	}
 }
