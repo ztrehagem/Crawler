a73f94e333c824f9c07cb193c8d8fdab3281f36f
MHz <megahertz.556@gmail.com>
2016-01-30 12:04:34 +0900

    リファクタリング

--- src/crawler2/CSSModifier.java
+++ src/crawler2/CSSModifier.java
@@ -6,6 +6,7 @@ class CSSModifier {
 	}
 
 	static String modify( Brain brain, String url, String line ) {
+
 		final StringBuilder sb = new StringBuilder();
 		int last = 0;
 		int head = 0;
@@ -15,6 +16,7 @@ class CSSModifier {
 
 			final char quotechar = line.charAt( head + 4 );
 			final boolean quote = quotechar == '\'' || quotechar == '"';
+
 			final int start = head + 4 + (quote ? 1 : 0);
 			final int end = line.indexOf( ")", start ) - (quote ? 1 : 0);
 
@@ -26,16 +28,15 @@ class CSSModifier {
 			last = end + 1 + (quote ? 1 : 0);
 
 			final String target = line.substring( start, end );
-
 			final String ext = StrUtil.getExtension( target );
-
 			final String fullpath = StrUtil.makeFullPath( url, target );
 
 			if( brain.f.makeID( fullpath, ext ) )
 				brain.t.offer( new FileSaveRunner( brain, fullpath ) );
 
-			sb.append( "url(\"" + brain.f.getFileName( fullpath ) + "\")" );
+			sb.append( "url(\"" ).append( brain.f.getFileName( fullpath ) ).append( "\")" );
 		}
+
 		sb.append( line.substring( last ) );
 
 		return sb.toString();
